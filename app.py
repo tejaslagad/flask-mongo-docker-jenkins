1e9109b8dcc834f66aff5ebb5e852ea2be8078cf
Tejas Lagad <tejaslagad38433@gmail.com>
2025-06-19 10:19:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
+import logging
+
+app = Flask(__name__)
+
+client = MongoClient(os.environ.get("MONGO_URI"))
+db = client.get_database()
+
+# Logging
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+logging.basicConfig(filename='logs/app.log', level=logging.INFO)
+
+@app.route('/')
+def index():
+    logging.info("API accessed at / endpoint")
+    return jsonify({"message": "Flask Mongo API Running Successfully"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
